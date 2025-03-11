602280856279b42c697d80dab4078d3a64f8d281
Matheus Gois <matheusgois.pessoal@gmail.com>
2025-03-11 01:47:10 -0300

    sem saber que o pra sempre ... sempre acaba

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -11,3 +11,22 @@ function obterData() {
 
 //executar a função ao iniciar o aplicativo(janela principal)
 document.getElementById('dataAtual').innerHTML = obterData()
+
+function addService() {
+    const servico = document.querySelector("#servicos")
+    servico.insertAdjacentHTML("beforebegin", 
+    `
+    <div class="mt-1 row g-2">
+        <div class="col-md-9">
+            <input type="text" placeholder="Descrição do serviço" class="form-control" spellcheck="false" maxlength="30">
+        </div>
+        <div class="col-md-2">
+            <input type="number" placeholder="Preço" class="form-control" spellcheck="false" min="0.00" max="99999.99">
+        </div>
+    </div>
+    `
+    )
+}
+
+const deletar = document.querySelector("#btnRegister")
+deletar.className.replace("btn btn-danger")
